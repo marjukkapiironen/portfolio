9e94b7a4a3cd903f4748125032b8f022a5220ff7
Marjukka <emapi007@edu.xamk.fi>
2024-02-05 01:09:21 +0200

    changed meta theme-color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="theme-color" content="#f2f8fc" />
       <link rel="icon" href="./favicon.ico" sizes="any" />
       <body>{children}</body>
     </html>
